3cd0eabb4da339bef74e89bb6769404d002561c4
Stef Simons <r0672078@student.thomasmore.be>
2023-01-03 21:28:01 +0100

    ViewModels and Views added

---
+++ Simons_Stef_Ga21/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simons_Stef_Ga21.ViewModel
+{
+    class MainViewModel
+    {
+    }
+}
